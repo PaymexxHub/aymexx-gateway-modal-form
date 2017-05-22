fe7b5f9144e60586c24d73b96bfb859bad97d51d
PaymexxHub <developer@zercomsystems.com>
2017-05-22 16:13:02 +0100

    Update functions.php
    Changed encryption method for total amount

--- functions.php
+++ functions.php
@@ -1,18 +1,13 @@
 <?php
 
-   function encrypt3Des($data, $key) {
-        //Generate a key from a hash
-        $key = md5(utf8_encode($key), true);
-        //Take first 8 bytes of $key and append them to the end of $key.
-        $key .= substr($key, 0, 8);
-        //Pad for PKCS7
-        $blockSize = mcrypt_get_block_size('tripledes', 'ecb');
-        $len = strlen($data);
-        $pad = $blockSize - ($len % $blockSize);
-        $data = $data . str_repeat(chr($pad), $pad);
-       //Encrypt data
-        $encData = mcrypt_encrypt('tripledes', $key, $data, 'ecb');
-       //return $this->strToHex($encData);
-      return base64_encode($encData);
+   function encryptAmount($data,$secret_key){
+        $algorithm = 'AES-256-CBC';
+        $skey = explode('-',$secret_key);
+        $secret = $skey[1];
+        $iv = substr($secret, 0, 16);
+        $encrypted = base64_encode($iv) . openssl_encrypt($data, $algorithm, $secret, 0, $iv);
+        $hmac = hash_hmac('md5', $encrypted, $secret);
+        return $encrypted.'-'.$hmac;
     }
+    
 
